6f86dd8af5e1cae2f87960f6cf6984c2418637eb
vishal-blok <vishal@blokument.com>
2023-12-05 10:32:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// write the palindrom program
+
+// const palindrom = (ar) => {
+//     const original = ar.split(' ').reverse().join('');
+//     if (original === ar) {
+//       return 'palindrom';
+//     } else {
+//       return 'not Palindrom';
+//     }
+//   };
+  
+//   console.log(palindrom('lool'));
+
+//   Find the largest three elements in an array
+//  const LargestNo = (b) =>{
+//   const d = b.sort((a,b)=> a - b)
+//   const largestN = d.slice(-3);
+//   console.log(largestN)
+//  }
+//  console.log(LargestNo([1,2,5,8,7,6,9]));
+
+//  merge 2 array
+var a = [1,2,3,4]
+var b = [5,6,7,8]
+
+const c = a.concat(b);
+console.log(c);
+ 
